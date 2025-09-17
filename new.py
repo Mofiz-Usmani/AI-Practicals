5babfe572fd2435421fd29bf7046646f8d297e41
Jackal <mofizusmani1@gmail.com>
2025-09-17 13:24:50 +0530

    new file added

--- new.py
+++ new.py
@@ -1,3 +1,4 @@
+# Practical 1(a): 
 def dfs_paths(graph, start, goal, path=None):
     if path is None:
         path = [start]
@@ -19,5 +20,46 @@ graph = {
     'F': set()
 }
 
-# Find all paths from C to F 
+# Find all paths from C to F
 print(list(dfs_paths(graph, 'C', 'F')))
+
+
+
+
+
+
+# Practical 1(b):
+##from collections import deque
+##
+##def bfs_paths(graph, start, goal):
+##    queue = deque([[start]])  # Each element is a path
+##    paths = []
+##
+##    while queue:
+##        path = queue.popleft()
+##        node = path[-1]
+##
+##        if node == goal:
+##            paths.append(path)
+##
+##        for neighbor in graph.get(node, []):
+##            if neighbor not in path:  # Avoid cycles
+##                queue.append(path + [neighbor])
+##
+##    return paths
+##
+##
+### Example graph
+##graph = {
+##    'A': ['B', 'C'],
+##    'B': ['D', 'E'],
+##    'C': ['F', 'A'],
+##    'D': [],
+##    'E': ['F'],
+##    'F': []
+##}
+##
+### Find all paths from C to F
+##print(bfs_paths(graph, 'C', 'F'))
+
+
