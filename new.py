a197b3f13a9e1499445a06573084ac61ad58aa01
Jackal <mofizusmani1@gmail.com>
2025-09-22 19:48:42 +0530

    practical 1 to 3

--- new.py
+++ new.py
@@ -1,65 +1,11 @@
-# Practical 1(a): 
-def dfs_paths(graph, start, goal, path=None):
-    if path is None:
-        path = [start]
+import random
 
-    if start == goal:
-        yield path
+def hill_climb(f, x, steps=100):
+    for _ in range(steps):
+        n = x + random.choice([-1,1])     # neighbor
+        if f(n) > f(x): x = n             # move if better
+    return x
 
-    for next in graph[start] - set(path):
-        yield from dfs_paths(graph, next, goal, path + [next])
-
-
-# Example graph (adjacency as sets for easier difference operation)
-graph = {
-    'A': {'B', 'C'},
-    'B': {'D', 'E'},
-    'C': {'F', 'A'},
-    'D': set(),
-    'E': {'F'},
-    'F': set()
-}
-
-# Find all paths from C to F
-print(list(dfs_paths(graph, 'C', 'F')))
-
-
-
-
-
-
-# Practical 1(b):
-##from collections import deque
-##
-##def bfs_paths(graph, start, goal):
-##    queue = deque([[start]])  # Each element is a path
-##    paths = []
-##
-##    while queue:
-##        path = queue.popleft()
-##        node = path[-1]
-##
-##        if node == goal:
-##            paths.append(path)
-##
-##        for neighbor in graph.get(node, []):
-##            if neighbor not in path:  # Avoid cycles
-##                queue.append(path + [neighbor])
-##
-##    return paths
-##
-##
-### Example graph
-##graph = {
-##    'A': ['B', 'C'],
-##    'B': ['D', 'E'],
-##    'C': ['F', 'A'],
-##    'D': [],
-##    'E': ['F'],
-##    'F': []
-##}
-##
-### Find all paths from C to F
-##print(bfs_paths(graph, 'C', 'F'))
-
-
+# Example: maximize f(x) = -(x-3)**2 + 9 (peak at x=3)
+f = lambda x: -(x-3)**2 + 9
+print("Best:", hill_climb(f, random.randint(-10,10)))
